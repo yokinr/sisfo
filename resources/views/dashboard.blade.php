c31581b8af9d22f7d235ceba618f9b3a8b6f1764
yoki <ynrestu@hotmail.com>
2023-09-02 20:32:21 +0700

    292023

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,8 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                    {{ auth()->user()->peran_id_str }}
+                    {{-- {{ __("You're logged in!") }} --}}
+                    <div class="font-bold text-2xl mb-3">{{ auth()->user()->name }}</div>
+                    <div class="bg-teal-100 p-2 border rounded">
+                        Silahkan unggah dokumen anda <a wire:navigate href="{{ route('dokument') }}"><b>disini!</b></a>
+                    </div>
                 </div>
             </div>
         </div>
