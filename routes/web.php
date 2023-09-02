4465e8d95c7ff153d19ffad521eb5de73abfdb68
yoki <ynrestu@hotmail.com>
2023-09-02 20:39:45 +0700

    292023

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\ProfileController;
 use App\Livewire\Coba;
 use App\Livewire\DokumentSiswa;
@@ -19,9 +20,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthenticatedSessionController::class, 'create']);
 
 
 
