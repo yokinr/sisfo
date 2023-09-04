cae82eef6a5d39362e13a2055fce5e531af8d137
yoki <ynrestu@hotmail.com>
2023-09-04 18:55:11 +0700

    4923

--- resources/views/components/loading.blade.php
+++ resources/views/components/loading.blade.php
@@ -1,3 +1,4 @@
-<div class="fixed top-0 left-0 w-full h-screen bg-black bg-opacity-50 flex items-center justify-center" wire:loading>
+<div class="fixed top-0 left-0 w-full h-screen bg-black bg-opacity-50 flex items-center justify-center z-60"
+    wire:loading.flex>
     <img src="{{ url('sisfo/public/img/spinner.gif') }}" class="rounded w-32 object-contain object-center">
 </div>
