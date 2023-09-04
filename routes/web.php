756a18733a6c8489e263f9cc43b0a4794fdeb50d
yoki <ynrestu@hotmail.com>
2023-09-04 16:19:05 +0700

    4923

--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthenticatedSessionController::class, 'create'])->name('login');
 
 Route::get('pembelajaran', PembelajaranComponent::class)->name('guest.pembelajaran');
 Route::get('pembelajaran/gtk/{ptk_id}/{semester_id}', PembelajaranGtkComponent::class)->name('guest.pembelajaran.gtk');
